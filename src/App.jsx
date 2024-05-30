2db97460dfe5d4faa828a8cc7a4eede38bf6a854
Swift Media <swiftmedia90@gmail.com>
2024-05-30 07:42:45 +0100

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignOut from "./pages/SignOut";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
 function App() {
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={"Page not found"} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
